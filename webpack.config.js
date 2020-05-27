e37d6144767ab16fa11ec9d4851f4da2d3ef537a
Nico Braun <nico.braun@live.de>
2020-05-27 01:52:37 +0000

    clean up structure

--- webpack.config.js
+++ webpack.config.js
@@ -17,25 +17,25 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Dashboard',
-      template: './src/shared/baseof.html',
+      template: './src/shared/html/baseof.html',
       filename: 'index.html',
       chunks: ['index'],
     }),
     new HtmlWebpackPlugin({
       title: 'Script Runner',
-      template: './src/shared/baseof.html',
+      template: './src/shared/html/baseof.html',
       filename: 'load.html',
       chunks: ['load'],
     }),
     new HtmlWebpackPlugin({
       title: 'Web Shell',
-      template: './src/shared/baseof.html',
+      template: './src/shared/html/baseof.html',
       filename: 'shell.html',
       chunks: ['shell'],
     }),
     new HtmlWebpackPlugin({
       title: 'Query API',
-      template: './src/shared/baseof.html',
+      template: './src/shared/html/baseof.html',
       filename: 'query.html',
       chunks: ['query'],
     }),
@@ -43,22 +43,27 @@ module.exports = {
       {
         path: path.join('./src/index.html'),
         template_filename: 'index.html',
-        location: 'main'
+        location: 'main',
       },
       {
         path: path.join('./src/load/load.html'),
         template_filename: 'load.html',
-        location: 'main'
+        location: 'main',
       },
       {
         path: path.join('./src/shell/shell.html'),
         template_filename: 'shell.html',
-        location: 'main'
+        location: 'main',
       },
       {
         path: path.join('./src/query/query.html'),
         template_filename: 'query.html',
-        location: 'main'
+        location: 'main',
+      },
+      {
+        path: path.join('./src/shared/html/aside.html'),
+        template_filename: '*',
+        location: 'main',
       },
     ]),
   ],
