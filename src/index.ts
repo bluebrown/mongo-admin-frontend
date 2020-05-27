c4af64ab45b6329095a24896c7705cf509e9d1c0
Nico Braun <nico.braun@live.de>
2020-05-27 02:33:58 +0000

    toggler

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,20 @@ import './shared/styles/tailwind.css'
 import './index.css'
 
 
-document.getElementById('viewtoggle').onclick = () => {
-  document.getElementById('datatable').classList.toggle('gridview')
+let gb = document.getElementById('gridbutton')
+let lb = document.getElementById('listbutton')
+let t = document.getElementById('datatable')
+
+
+lb.onclick = () => {
+  lb.classList.add('active')
+  gb.classList.remove('active')
+  t.classList.remove('gridview')
+
+}
+gb.onclick = () => {
+  gb.classList.add('active')
+  lb.classList.remove('active')
+  t.classList.add('gridview')
+
 }
